d4f76de78d5e521154d406fe3e9468770ebf5f9f
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2023-07-05 14:47:41 +0200

    hamburger responsive width

--- js/menu-orizzontale2.js
+++ js/menu-orizzontale2.js
@@ -107,8 +107,6 @@ function manage_menu(){
 // SHOW / HIDE SOTTOMENU MEGAMENU 4 LIVELLI
 
  
-// Seleccionar todos los elementos con la clase deseada
-
 
 var menuItem = document.querySelectorAll('.menu-item.megamenu');
 
@@ -200,7 +198,6 @@ function showMegamenu4Livelli (targetElement){
       let submenuPanel = targetElement.querySelector(".submenu-panel");
       let allSubmenuPanels = document.querySelectorAll(".submenu-panel");
       let allSubmenuPanelsButThis = [];
-      // alert(targetElement.classList);
         
       for (let i = 0; i < allSubmenuPanels.length; i++) {
         if (allSubmenuPanels[i] !== submenuPanel) {
@@ -209,7 +206,6 @@ function showMegamenu4Livelli (targetElement){
       }
 
       for (var i = 0; i < allSubmenuPanelsButThis.length; i++){
-      // alert(allSubmenuPanelsButThis[i].classList);
       allSubmenuPanelsButThis[i].classList.remove("show-fade");
       }
     }
@@ -242,10 +238,7 @@ else if (window.innerWidth < screanWidthBreakpoint) {
 
   // Iterar sobre los elementos y agregar el event listener
   submenuItem.forEach(elemento => {
-    if (elemento.querySelector(".frecce-zone-a")){
-    
-      // alert(elemento.querySelector(".frecce-zone-a").classList);
-      
+    if (elemento.querySelector(".frecce-zone-a")){      
 
       elemento.querySelector(".frecce-zone-a").addEventListener(evento, () => {
         // Ejecutar la función y pasar el elemento clicado como argumento
@@ -331,14 +324,9 @@ function showSubSubMegamenu4Livelli(targetElement, panel) {
   
   if (window.innerWidth < screanWidthBreakpoint && !panel.classList.contains("full-height")) {
 
-/*     setTimeout(() => {
- */
       panel.classList.add("full-height");
-      // alert(panel.style.height);
 
-/*     }
-    ,timeOut);
- */  }
+  }
 
 vinculateArrows();
   
@@ -361,10 +349,7 @@ else if (window.innerWidth < screanWidthBreakpoint) {
   // Iterar sobre los elementos y agregar el event listener
   subSubmenuItem.forEach(elemento => {
     if (elemento.querySelector(".frecce-zone-a")){
-    
-      // alert(elemento.querySelector(".frecce-zone-a").classList);
-      
-
+          
       elemento.querySelector(".frecce-zone-a").addEventListener(evento, () => {
         // Ejecutar la función y pasar el elemento clicado como argumento
         showSubSubSubMegamenu4Livelli(elemento, elemento.closest(".submenu-panel-table"));
@@ -442,18 +427,11 @@ function showSubSubSubMegamenu4Livelli(targetElement, panel) {
   
   if (window.innerWidth < screanWidthBreakpoint && !panel.classList.contains("full-height")) {
 
-/*     setTimeout(() => {
- */
-      panel.classList.add("full-height");
-      // alert(panel.style.height);
+    panel.classList.add("full-height");
+  }
 
-/*     }
-    ,timeOut);
- */  }
-
-vinculateArrows();
+  vinculateArrows();
   
-
 }
 
 
@@ -486,7 +464,6 @@ else if (window.innerWidth < screanWidthBreakpoint) {
 }
 
 function showSubmenuTendina(targetElement){
-  // alert("function");
   let submenuPanel = targetElement.querySelector(".submenu-panel");
   let allSubmenuPanels = document.querySelectorAll(".submenu-panel");
   let allSubmenuPanelsButThis = [];
@@ -540,7 +517,6 @@ function showSubmenuTendina(targetElement){
     let allSubmenuPanels = document.querySelectorAll(".submenu-panel");
     let allSubmenuPanelsButThis = [];
     let submenuPanel = targetElement.querySelector(".submenu-panel");
-    // alert(submenuPanel.classList);
 
   
     for (let i = 0; i < allSubmenuPanels.length; i++) {
@@ -566,7 +542,6 @@ function showSubmenuTendina(targetElement){
 // SHOW / HIDE SOTTO SOTTO MENU TENDINA
 
 var submenuItemTendina = document.querySelectorAll('.menu-item.tendina .submenu-item');
-// alert(submenuItemTendina[0]);
 
 if (window.innerWidth >= screanWidthBreakpoint) {
 
@@ -586,8 +561,6 @@ else if (window.innerWidth < screanWidthBreakpoint) {
   submenuItemTendina.forEach(elemento => {
     if (elemento.querySelector(".frecce-zone-a")){
     
-      // alert(elemento.querySelector(".frecce-zone-a").classList);
-      // alert(elemento.closest(".submenu-panel").classList);
 
       elemento.querySelector(".frecce-zone-a").addEventListener(evento, () => {
         // Ejecutar la función y pasar el elemento clicado como argumento
@@ -599,7 +572,6 @@ else if (window.innerWidth < screanWidthBreakpoint) {
 }
 
 function showSubSubmenuTendina(targetElement, panel){
-  // alert("function");
 
   let subSubmenuTendinaPanel = targetElement.querySelector(".sub-submenu-tendina-panel");
   let allSubSubmenuTendinaPanels = document.querySelectorAll(".sub-submenu-tendina-panel");
@@ -636,7 +608,6 @@ function showSubSubmenuTendina(targetElement, panel){
   }
 
   let subSubmenu = subSubmenuTendinaPanel.querySelector(".sub-submenu-tendina");
-  // alert(subSubmenu.classList);
   let alturaPanel = subSubmenu.offsetHeight;
   document.documentElement.style.setProperty('--altura-panel', alturaPanel + "px");
 
@@ -725,7 +696,6 @@ function showSubmenuPanelTableCentered(panelTable, elementWithHas){
 
 
   
-  // alert("function show");
   var submenuPanelTableCenteredContainer = document.getElementById(panelTable);
   var submenuPanelTableCentered = submenuPanelTableCenteredContainer.querySelector(".submenu-panel-table-centered");
   let allSubmenuPanelTableCentered = document.querySelectorAll(".submenu-panel-table-centered");
@@ -762,7 +732,6 @@ function showSubmenuPanelTableCentered(panelTable, elementWithHas){
 
   let subSubmenu = submenuPanelTableCentered.querySelector(".sub-submenu");
   let alturaPanel = subSubmenu.offsetHeight;
-  // alert(alturaPanel);
   document.documentElement.style.setProperty('--altura-panel', alturaPanel + "px");
 
 
@@ -823,7 +792,6 @@ else if (window.innerWidth < screanWidthBreakpoint) {
   
 
 function showSubSubmenuCentered(targetElement, panel){
-  // alert("function");
 
   let subSubSubmenuPanel = targetElement.querySelector(".sub-sub-submenu-panel");
   let allSubSubSubmenuPanels = document.querySelectorAll(".sub-sub-submenu-panel");
@@ -860,7 +828,6 @@ function showSubSubmenuCentered(targetElement, panel){
   }
 
   let subSubSubmenu = subSubSubmenuPanel.querySelector(".sub-sub-submenu");
-  // alert(subSubmenu.classList);
   let alturaPanel = subSubSubmenu.offsetHeight;
   document.documentElement.style.setProperty('--altura-panel', alturaPanel + "px");
 
@@ -991,20 +958,16 @@ function vinculateArrows(){
 
   if (window.innerWidth < screanWidthBreakpoint) {
 
-    // alert("arrow")
     setTimeout(() => {
 
       let arrowAll = document.querySelectorAll(".frecce");
       var liElement = [];
-    // alert(liElement[0].classList);
 
 
       for (var i = 0; i < arrowAll.length; i++){
         liElement[i] = arrowAll[i].closest('li');
       
       }
-      // alert(liElement[0].classList);
-
 
       for (var i = 0; i < arrowAll.length; i++){
 
@@ -1035,14 +998,11 @@ function vinculateArrowsTableCentered(panelTable, elementWithHas){
 
   if (window.innerWidth < screanWidthBreakpoint) {
 
-    // alert("arrow")
     setTimeout(() => {
 
       let arrowAll = document.querySelectorAll(".frecce");
       var submenuPanelTableCenteredContainer = document.getElementById(panelTable);
-      var submenuPanelTableCentered = submenuPanelTableCenteredContainer.querySelector(".submenu-panel-table-centered");
-      // alert(submenuPanelTableCentered.classList);
-    
+      var submenuPanelTableCentered = submenuPanelTableCenteredContainer.querySelector(".submenu-panel-table-centered");    
 
 
       for (var i = 0; i < arrowAll.length; i++){
